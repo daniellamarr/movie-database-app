879cd118d200e85d330097704447b36ffa5562c6
timzprof <timilehin.olumofin@gmail.com>
2020-01-24 01:08:54 +0100

    Added Project Setup Files and Configurations

---
+++ src/util/api.js
@@ -0,0 +1,21 @@
+import { create } from 'axios';
+
+/**
+ * Interacts with the Movie DB API
+ * @param {Object} requestConfig Request configuration
+ * @param {'post'|'get'|'delete'|'put'} requestConfig.method HTTP verb
+ * @param {String} requestConfig.url HTTP verb
+ * @param {Object} requestConfig.headers HTTP http header
+ * @param {Object} requestConfig.data data to send to backend
+ * @return {Promise<{ data, headers, status, statusText, config }>}
+ */
+export const movieDbServiceClient = requestConfig => create({
+  baseURL: `https://api.themoviedb.org/3/movie`,
+  timeout: 150000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  params: {
+      api_key: process.env.MOVIE_DB_API_KEY
+  }
+})(requestConfig);
