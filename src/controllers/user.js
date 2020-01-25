6b9aed46db76d3c9615e5e8e3d709e0f3e286c26
timzprof <timilehin.olumofin@gmail.com>
2020-01-25 04:50:32 +0100

    Added Movie ID Validation Requests

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,5 @@
-import {trimUser} from '../util/user';
+import {trimUser} from "../util/user";
+import {movieDbServiceClient} from "../util/api";
 
 /**
  * User Controller Initialization Function
@@ -7,31 +8,50 @@ import {trimUser} from '../util/user';
  * @returns {Object} Controller Object
  */
 export default ({userModel}) => {
-    const addToWatchlist = async (req, res, next) => {
-        try {
-            const user = await userModel.findOne({where: {email: req.user.email}});
+	const addToWatchlist = async (req, res, next) => {
+		try {
+			const user = await userModel.findOne({where: {email: req.user.email}});
 
-            if(!user){
-                const userNotFoundError = new Error(`User with email - ${req.user.email} Not Found`);
-                userNotFoundError.statusCode = 404;
-                throw userNotFoundError;
-            }
+			if (!user) {
+				const userNotFoundError = new Error(
+					`User with email - ${req.user.email} Not Found`
+				);
+				userNotFoundError.statusCode = 404;
+				throw userNotFoundError;
+			}
 
-            const updatedUser = await user.update({
-                watchlist: [...user.dataValues.watchlist, req.body.movieId] 
-            });
+			try {
+				await movieDbServiceClient({
+					url: `/${req.body.movieId}`,
+					method: "get"
+				});
+			} catch (error) {
+				if (error?.response?.data?.status_message.includes("could not be found")) {
+					const movieNotFoundError = new Error(
+						"A movie with that ID can't be found"
+					);
+					movieNotFoundError.statusCode = 404;
+					throw movieNotFoundError;
+				} else {
+					throw error;
+				}
+			}
 
-            const trimmedUser = trimUser(updatedUser);
+			const updatedUser = await user.update({
+				watchlist: [...user.dataValues.watchlist, req.body.movieId]
+			});
 
-            return res.status(200).json({
-                status: 'success',
-                message: 'Movie Added to Watchlist',
-                data: trimmedUser
-            });
-        } catch (error) {
-            if (!error.statusCode) error.statusCode = 500;
+			const trimmedUser = trimUser(updatedUser);
+
+			return res.status(200).json({
+				status: "success",
+				message: "Movie Added to Watchlist",
+				data: trimmedUser
+			});
+		} catch (error) {
+			if (!error.statusCode) error.statusCode = 500;
 			return next(error);
-        }
-    };
-    return {addToWatchlist};
+		}
+	};
+	return {addToWatchlist};
 };
