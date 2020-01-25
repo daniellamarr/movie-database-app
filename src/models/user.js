25fe80cc03260b9054d3888cf3c10734712b9df5
timzprof <timilehin.olumofin@gmail.com>
2020-01-25 20:36:48 +0100

    Implemented User can review movie endpoint

--- src/models/user.js
+++ src/models/user.js
@@ -3,9 +3,10 @@
  * @param  {Object} ModelParams - Model Parameters
  * @param  {Object} ModelParams.Sequelize - Sequelize
  * @param  {Object} ModelParams.db - Database Connection Object
+ * @param {Object} ModelParams.Review - Initialized Review Model
  * @returns {Object} SequelizeModel
  */
-export default ({Sequelize, db}) => {
+export default ({Sequelize, db, Review}) => {
 	const User = db.define("user", {
 		id: {
 			type: Sequelize.INTEGER,
@@ -29,12 +30,9 @@ export default ({Sequelize, db}) => {
 		watchlist: {
 			type: Sequelize.ARRAY(Sequelize.INTEGER),
             defaultValue: []
-		},
-		ratedMovies: {
-			type: Sequelize.ARRAY(Sequelize.INTEGER),
-            defaultValue: []
 		}
 	});
 
+	User.hasMany(Review, {constraints: true, onDelete: 'CASCADE'});
 	return User;
 };
