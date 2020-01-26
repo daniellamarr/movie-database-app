c4c64e0f747eeacfe364530fa3b20db5953134c2
kidmeeno <arthurbosah@yahoo.com>
2020-01-26 02:55:01 -0600

    removed comments

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -5,7 +5,6 @@ import MovieCard from '../../components/movieCard/movieCard';
 const HomePage = () => {
     return (
         <div>
-            Home Page Goes Here
             <MovieCard/>
         </div>
     )
