7e15ba467b9d10dcdb21751f2b745b0778e2d6a4
kidmeeno <arthurbosah@yahoo.com>
2020-01-26 04:01:13 -0600

    switched over from functional component to class component

--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -1,198 +1,210 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import './movieCard.scss';
 import movieImg from '../../assets/images/hiclipart.com.png';
 import StarRating from '../starRating/starRating';
 import playIcon from '../../assets/images/play.svg';
 import seeMore from '../../assets/images/addMore.svg';
 
-const MovieCard = () => {
-    const [mouseHover, setMouseHover] = useState(0)
-
-    const particularCard = position => position === mouseHover
-
-    return (
-        <div className={"movie__card__wrapper"}>
-            <div onMouseOver={() => { setMouseHover(1) }} onMouseLeave={() => { setMouseHover(0) }}>
-                {
-                    particularCard(1) ?
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail__on__hover"}>
-                                <div>
-                                    <span>MOVIES</span>
-                                    <p>The Source</p>
-                                    <div className={"card__btn"}>
-                                        <button>COMEDY</button>
-                                        <button>DRAMA</button>
+class MovieCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            mouseHover: 0
+        }
+    }
+    checkForHoverdCard(card){
+        if(card === this.state.mouseHover){
+            return true
+        }else{
+            return false
+        }
+    }
+    render() {
+        return (
+            <div className={"movie__card__wrapper"}>
+                <div onMouseOver={() => { this.setState({ mouseHover: 1 }) }} onMouseLeave={() => { this.setState({ mouseHover: 0 }) }}>
+                    {
+                        this.checkForHoverdCard(1) ?
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail__on__hover"}>
+                                    <div>
+                                        <span>MOVIES</span>
+                                        <p>The Source</p>
+                                        <div className={"card__btn"}>
+                                            <button>COMEDY</button>
+                                            <button>DRAMA</button>
+                                        </div>
+                                    </div>
+                                    <div className={"play"}>
+                                        <img src={playIcon} alt="play" />
+                                    </div>
+                                    <div className={"seeMore"}>
+                                        <StarRating index={0} />
+                                        <img src={seeMore} alt="seemore" />
                                     </div>
                                 </div>
-                                <div className={"play"}>
-                                    <img src={playIcon} alt="play" />
-                                </div>
-                                <div className={"seeMore"}>
-                                    <StarRating index={0} />
-                                    <img src={seeMore} alt="seemore"/>
-                                </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                        :
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail"}>
-                                <span>MOVIES</span>
-                                <p>The Source</p>
-                                <div>
-                                    <StarRating index={0} />
-                                </div>
-                            </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                }
-            </div>
-            <div onMouseOver={() => { setMouseHover(2) }} onMouseLeave={() => { setMouseHover(0) }}>
-                {
-                    particularCard(2) ?
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail__on__hover"}>
-                                <div>
+                            :
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail"}>
                                     <span>MOVIES</span>
                                     <p>The Source</p>
-                                    <div className={"card__btn"}>
-                                        <button>COMEDY</button>
-                                        <button>DRAMA</button>
+                                    <div>
+                                        <StarRating index={0} />
                                     </div>
                                 </div>
-                                <div className={"play"}>
-                                    <img src={playIcon} alt="play" />
-                                </div>
-                                <div className={"seeMore"}>
-                                    <StarRating index={0} />
-                                    <img src={seeMore} alt="seemore"/>
-                                </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                        :
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail"}>
-                                <span>MOVIES</span>
-                                <p>The Source</p>
-                                <div>
-                                    <StarRating index={0} />
+                    }
+                </div>
+                <div onMouseOver={() => { this.setState({ mouseHover: 2 }) }} onMouseLeave={() => { this.setState({ mouseHover: 0 }) }}>
+                    {
+                        this.checkForHoverdCard(2) ?
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail__on__hover"}>
+                                    <div>
+                                        <span>MOVIES</span>
+                                        <p>The Source</p>
+                                        <div className={"card__btn"}>
+                                            <button>COMEDY</button>
+                                            <button>DRAMA</button>
+                                        </div>
+                                    </div>
+                                    <div className={"play"}>
+                                        <img src={playIcon} alt="play" />
+                                    </div>
+                                    <div className={"seeMore"}>
+                                        <StarRating index={0} />
+                                        <img src={seeMore} alt="seemore" />
+                                    </div>
                                 </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                }
-            </div>
-            <div onMouseOver={() => { setMouseHover(3) }} onMouseLeave={() => { setMouseHover(0) }}>
-                {
-                    particularCard(3) ?
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail__on__hover"}>
-                                <div>
+                            :
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail"}>
                                     <span>MOVIES</span>
                                     <p>The Source</p>
-                                    <div className={"card__btn"}>
-                                        <button>COMEDY</button>
-                                        <button>DRAMA</button>
+                                    <div>
+                                        <StarRating index={0} />
                                     </div>
                                 </div>
-                                <div className={"play"}>
-                                    <img src={playIcon} alt="play" />
-                                </div>
-                                <div className={"seeMore"}>
-                                    <StarRating index={0} />
-                                    <img src={seeMore} alt="seemore"/>
-                                </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                        :
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail"}>
-                                <span>MOVIES</span>
-                                <p>The Source</p>
-                                <div>
-                                    <StarRating index={0} />
+                    }
+                </div>
+                <div onMouseOver={() => { this.setState({ mouseHover: 3 }) }} onMouseLeave={() => { this.setState({ mouseHover: 0 }) }}>
+                    {
+                        this.checkForHoverdCard(3) ?
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail__on__hover"}>
+                                    <div>
+                                        <span>MOVIES</span>
+                                        <p>The Source</p>
+                                        <div className={"card__btn"}>
+                                            <button>COMEDY</button>
+                                            <button>DRAMA</button>
+                                        </div>
+                                    </div>
+                                    <div className={"play"}>
+                                        <img src={playIcon} alt="play" />
+                                    </div>
+                                    <div className={"seeMore"}>
+                                        <StarRating index={0} />
+                                        <img src={seeMore} alt="seemore" />
+                                    </div>
                                 </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                }
-            </div>
-            <div onMouseOver={() => { setMouseHover(4) }} onMouseLeave={() => { setMouseHover(0) }}>
-                {
-                    particularCard(4) ?
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail__on__hover"}>
-                                <div>
+                            :
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail"}>
                                     <span>MOVIES</span>
                                     <p>The Source</p>
-                                    <div className={"card__btn"}>
-                                        <button>COMEDY</button>
-                                        <button>DRAMA</button>
+                                    <div>
+                                        <StarRating index={0} />
                                     </div>
                                 </div>
-                                <div className={"play"}>
-                                    <img src={playIcon} alt="play" />
-                                </div>
-                                <div className={"seeMore"}>
-                                    <StarRating index={0} />
-                                    <img src={seeMore} alt="seemore"/>
-                                </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                        :
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail"}>
-                                <span>MOVIES</span>
-                                <p>The Source</p>
-                                <div>
-                                    <StarRating index={0} />
+                    }
+                </div>
+                <div onMouseOver={() => { this.setState({ mouseHover: 4 }) }} onMouseLeave={() => { this.setState({ mouseHover: 0 }) }}>
+                    {
+                        this.checkForHoverdCard(4) ?
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail__on__hover"}>
+                                    <div>
+                                        <span>MOVIES</span>
+                                        <p>The Source</p>
+                                        <div className={"card__btn"}>
+                                            <button>COMEDY</button>
+                                            <button>DRAMA</button>
+                                        </div>
+                                    </div>
+                                    <div className={"play"}>
+                                        <img src={playIcon} alt="play" />
+                                    </div>
+                                    <div className={"seeMore"}>
+                                        <StarRating index={0} />
+                                        <img src={seeMore} alt="seemore" />
+                                    </div>
                                 </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                }
-            </div>
-            <div onMouseOver={() => { setMouseHover(5) }} onMouseLeave={() => { setMouseHover(0) }}>
-                {
-                    particularCard(5) ?
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail__on__hover"}>
-                                <div>
+                            :
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail"}>
                                     <span>MOVIES</span>
                                     <p>The Source</p>
-                                    <div className={"card__btn"}>
-                                        <button>COMEDY</button>
-                                        <button>DRAMA</button>
+                                    <div>
+                                        <StarRating index={0} />
                                     </div>
                                 </div>
-                                <div className={"play"}>
-                                    <img src={playIcon} alt="play" />
-                                </div>
-                                <div className={"seeMore"}>
-                                    <StarRating index={0} />
-                                    <img src={seeMore} alt="seemore"/>
-                                </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                        :
-                        <div className={"movie__card"}>
-                            <div className={"movie__detail"}>
-                                <span>MOVIES</span>
-                                <p>The Source</p>
-                                <div>
-                                    <StarRating index={0} />
+                    }
+                </div>
+                <div onMouseOver={() => { this.setState({ mouseHover: 5 }) }} onMouseLeave={() => { this.setState({ mouseHover: 0 }) }}>
+                    {
+                        this.checkForHoverdCard(5) ?
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail__on__hover"}>
+                                    <div>
+                                        <span>MOVIES</span>
+                                        <p>The Source</p>
+                                        <div className={"card__btn"}>
+                                            <button>COMEDY</button>
+                                            <button>DRAMA</button>
+                                        </div>
+                                    </div>
+                                    <div className={"play"}>
+                                        <img src={playIcon} alt="play" />
+                                    </div>
+                                    <div className={"seeMore"}>
+                                        <StarRating index={0} />
+                                        <img src={seeMore} alt="seemore" />
+                                    </div>
                                 </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
                             </div>
-                            <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
-                        </div>
-                }
+                            :
+                            <div className={"movie__card"}>
+                                <div className={"movie__detail"}>
+                                    <span>MOVIES</span>
+                                    <p>The Source</p>
+                                    <div>
+                                        <StarRating index={0} />
+                                    </div>
+                                </div>
+                                <img src={movieImg} alt="Gogetor" width="100%" className={"movie__img"} />
+                            </div>
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
 export default MovieCard
--- src/components/starRating/starRating.js
+++ src/components/starRating/starRating.js
@@ -1,62 +1,18 @@
-import React,{useState, useEffect} from 'react';
+import React,{Component} from 'react';
 import './starRating.scss'
-
-const StarRating = (props) => {
-    const [state, setState] = useState({
-        selected : false,
-        selectStar: 0,
-    })
-    const handleStarToggle = (length, add) => {
-        for (let index = 1; index <= length; index++) {
-            var id = `star${index}`;
-            var currentDocument = document.getElementById(id);
-            if (currentDocument) {
-                if (add) {
-                    currentDocument.classList.add('checked');
-                } else if (state.selected && (parseInt(state.selectedStar) < index)) {
-                    currentDocument.classList.remove('checked');
-                } else if (!state.selectStar) {
-                    currentDocument.classList.remove('checked');
-                } else if (state.selected && (parseInt(state.selectedStar) >= index)) {
-                } else {
-                    currentDocument.classList.remove('checked');
-                }
-            }
-        }
+class StarRating extends Component {
+    render () {
+        return (
+            <div className="rating__star">
+                <div className="fa fa-star star-size checked" id="star1" data-key="1"></div>
+                <div className="fa fa-star star-size checked" id="star2" data-key="2" ></div>
+                <div className="fa fa-star star-size checked" id="star3" data-key="3" ></div>
+                <div className="fa fa-star star-size checked" id="star4" data-key="4" ></div>
+                <div className="fa fa-star star-size" id="star5" data-key="5" ></div>
+            </div>
+        )
+    
     }
-    const handlehover = (currentValue) => {
-        if (currentValue.target) {
-            var length = currentValue.target.attributes["data-key"].value;
-            handleStarToggle(length, true)
-        }
-    }
-    const handleMouseLeave = (currentValue) => {
-        if (currentValue.target) {
-            var length = currentValue.target.attributes["data-key"].value;
-            handleStarToggle(length)
-        }
-    }
-    const selectStar = (currentStar) => {
-        currentStar.persist()
-        var length = currentStar.target.attributes["data-key"].value;
-        setState(...state, {selected: true, selectedStar: length }, () => {
-            handlehover(currentStar);
-            handleStarToggle(length);
-        })
-    }
-    return (
-        <div className="rating__star">
-            <div className="fa fa-star star-size checked" id="star1" data-key="1"></div>
-            <div className="fa fa-star star-size checked" id="star2" data-key="2" ></div>
-            <div className="fa fa-star star-size checked" id="star3" data-key="3" ></div>
-            <div className="fa fa-star star-size checked" id="star4" data-key="4" ></div>
-            <div className="fa fa-star star-size" id="star5" data-key="5" ></div>
-            {/* <div className="fa fa-star star-size" id="star1" data-key="1" onClick={selectStar} onMouseLeave={handleMouseLeave} onMouseOver={handlehover}></div>
-            <div className="fa fa-star star-size" id="star2" data-key="2" onClick={selectStar} onMouseOver={handlehover} onMouseLeave={handleMouseLeave}></div>
-            <div className="fa fa-star star-size" id="star3" data-key="3" onClick={selectStar} onMouseOver={handlehover} onMouseLeave={handleMouseLeave}></div>
-            <div className="fa fa-star star-size" id="star4" data-key="4" onClick={selectStar} onMouseOver={handlehover} onMouseLeave={handleMouseLeave}></div>
-            <div className="fa fa-star star-size" id="star5" data-key="5" onClick={selectStar} onMouseOver={handlehover} onMouseLeave={handleMouseLeave}></div> */}
-        </div>
-    )
 }
+
 export default StarRating
