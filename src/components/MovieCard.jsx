e0ab159e891b7989ea831f7dd9c1b051432afe59
timzprof <timilehin.olumofin@gmail.com>
2020-01-27 08:11:04 +0100

    Inegrated Get Latest Movies API Endpoint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,12 +5,13 @@ import Rating from './Rating';
 export default class MovieCard extends Component {
   render() {
     const {movie} = this.props;
+    const rating = Math.floor((movie.vote_average / 10) * 5);
     return (
-      <div className="movie-card" style={{backgroundImage: `url(${movie.photo})`}}>
+      <div className="movie-card" style={{backgroundImage: `url(${movie.poster_path})`}}>
         <div className="movie-card-title">
-          <Text fontSize={10} color="#fff">{movie.genre}</Text>
+          <Text fontSize={10} color="#fff">{movie.genres[0].name}</Text>
           <Text fontSize={16} color="#fff">{movie.title}</Text>
-          <Rating noOfStars={movie.rating} checkType="checked" />
+          <Rating noOfStars={rating} checkType="checked" />
         </div>
       </div>
     )
