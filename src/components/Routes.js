61bf002b40cc7b9dfc7facc0a5388df15ce80506
unknown <arthurbosah@yahoo.com>
2020-02-04 20:47:52 -0600

    done with the user profile page with watchlist card, made to changes to header to regards to routing a user to user profile

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -4,11 +4,13 @@ import { Home, Signup } from "../routes/";
 import SingleMovie from "../routes/SingleMovie";
 import Login from "../routes/Login";
 import NotFound from "../routes/NotFound";
+import UserProfile from "../routes/UserProfile";
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/movies/:movieId" component={SingleMovie} />
+    <Route exact path="/userprofile" component={UserProfile} />
     <Route exact path="/signup" component={Signup} />
     <Route exact path="/login" component={Login} />
     <Route component={NotFound} />
